b3806eea26879a1a136fa8a6e6e6fcbf3e154f7e
João Pedro Ribeiro Gonçalves <108089588+jpribeirogoncalves@users.noreply.github.com>
2023-06-22 20:06:11 -0300

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://arteemusica.mus.br/sistema/grupo02/sitebagdex/public/';
+    public string $baseURL = 'https://sitebagdex-production.up.railway.app/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
