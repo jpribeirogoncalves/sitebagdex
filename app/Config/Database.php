6e84b10269de2a7ecef08a574b80d4f3d8c9be46
João Pedro Ribeiro Gonçalves <108089588+jpribeirogoncalves@users.noreply.github.com>
2023-06-22 19:54:56 -0300

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'      => '',
-        'hostname' => 'localhost',
-        'username' => 'c2grupo02',
-        'password' => 'grupo02univicosa',
-        'database' => 'c2grupo02',
+        'hostname' => 'containers-us-west-117.railway.app',
+        'username' => 'railway',
+        'password' => 'PQolO4RKNsCAe0kPxMX1',
+        'database' => 'railway',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
@@ -41,7 +41,7 @@ class Database extends Config
         'compress' => false,
         'strictOn' => false,
         'failover' => [],
-        'port'     => 3306,
+        'port'     => 7363,
     ];
 
     /**
